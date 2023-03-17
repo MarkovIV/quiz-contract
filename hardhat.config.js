cc6b19c96603956988aa1b448eb6b17c9e79f36e
Ivan <mrkvvn@yandex.ru>
2023-03-17 15:46:56 +0300

    Code cleaning

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,64 +4,7 @@ require("dotenv").config()
 require("./tasks/tasks")
 
 module.exports = {
-  solidity: {
-    version: "0.8.17"
-  }
+	solidity: {
+		version: "0.8.17"
+	}
 }
-
-// module.exports = {
-//   defaultNetwork: "testnet",
-//   networks: {
-// 	localhost: {
-//       url: "http://127.0.0.1:8545"
-//     },
-//     hardhat: {
-// 		blockGasLimit: 30000,
-//       	accounts: {
-// 			count:100
-//       	}
-//     },
-// 	// BSC Testnet
-// 	testnet: {
-//       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-//       chainId: 97,
-//       gasPrice: 20000000000,
-//       accounts: [process.env.PRIVATE_KEY]
-//     },
-// 	// BSC Mainnet
-// 	mainnet: {
-//       url: "https://bsc-dataseed.binance.org/",
-//       chainId: 56,
-//       gasPrice: 20000000000,
-//       accounts: [process.env.PRIVATE_KEY]
-//     },
-//     goerli: {
-//       url: "https://goerli.infura.io/v3/4b584832ca034d68af836c6596e8e9e2",
-//       accounts: [process.env.PRIVATE_KEY]
-//     }
-//   },
-//   solidity: {
-//     version: "0.8.17",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }
-//   },
-//   paths: {
-//     sources: "./contracts",
-//     tests: "./test",
-//     cache: "./cache",
-//     artifacts: "./artifacts"
-//   },
-//   mocha: {
-//     timeout: 40000
-//   },
-//   etherscan: {
-// 	// For ETHERSCAN
-//     // apiKey: process.env.ETHERSCAN_API_KEY,
-// 	// For BSCSCAN
-// 	apiKey: process.env.BSCSCAN_API_KEY,
-//   }
-// }
